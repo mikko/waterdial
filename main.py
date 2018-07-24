f7df7c0db1eea88842d9db0276962b8051abea04
Mikko Pohja <mikko.pohja@futurice.com>
2018-07-24 22:50:32 +0300

    10s interval for testing

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from dial import get_litres
-
+from time import sleep
 import cv2
 
 cap = cv2.VideoCapture(0)
 
-# Scrap first frames to /dev/null for crappy cheap webcams doing
-# something
-for x in range(50):
-    ret, frame = cap.read()
-    if ret == False: # TODO: proper exit
-        print("No frame available")
+while True:
+    # Scrap first frames to /dev/null for crappy cheap webcams doing
+    # something
+    for x in range(50):
+        ret, frame = cap.read()
+        if ret == False: # TODO: proper exit
+            print("No frame available")
+            break
+
+    if ret == True:
+        meter_value = get_litres(frame)
+        print("Send value to backend")
+        print(meter_value)
+
+    if cv2.waitKey() & 0xFF == ord('q'):
         break
 
-if ret == True:
-	meter_value = get_litres(frame)
-	print("Send value to backend")
-	print(meter_value)
-
+    sleep(10)
 
 cap.release()
 cv2.destroyAllWindows()
